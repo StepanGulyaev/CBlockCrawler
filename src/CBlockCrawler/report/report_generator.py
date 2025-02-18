65f4efc405fbf341767c4786f9c7bb893b46b317
Stepan Gulyaev <stepan.gulyaev.02@gmail.com>
2025-02-18 21:45:15 +0300

    Coverage support added, but need to fix how it's done for program to be more organized

--- src/CBlockCrawler/report/report_generator.py
+++ src/CBlockCrawler/report/report_generator.py
@@ -4,14 +4,34 @@ from CBlockCrawler.cblock.cblock_handler import CBlock_handler
 from CBlockCrawler.funcs.src_functions_handler import Src_functions_handler
 
 class ReportGenerator(ABC):
-    def __init__(self, datafile_handler : CBlock_datafile_handler, report_name : str):
+    def __init__(self, datafile_handler : CBlock_datafile_handler, report_name : str, coverage_file : str):
         self.name = report_name
         self.fields = ["Source file","Id","Lines","Num of Lines","Functions inside","In function"]
+
+        if coverage_file is not None:
+            self.fields.append("Coverage")
+            coverage_collection = True
+        else: coverage_collection = False
+
         self.cblock_handlers = []
         for datafile in datafile_handler.datafiles:
             src_func_handler = Src_functions_handler(datafile.src_file)
-            cblock_handler = CBlock_handler(datafile, src_func_handler)
+            cblock_handler = CBlock_handler(datafile, src_func_handler,coverage_collection)
             self.cblock_handlers.append(cblock_handler)
+        
+        self.all_records = []
+        for handler in self.cblock_handlers:
+            one_file_records = []
+            for block in handler.blocks:
+                record = block.get_record()
+                if coverage_collection:
+                    record.append(block.get_block_coverage(coverage_file))
+                one_file_records.append(record)
+            self.all_records.append(one_file_records)
+
+
+
+
 
     def generate_report(self):
         pass
--- src/CBlockCrawler/report/txt_report_generator.py
+++ src/CBlockCrawler/report/txt_report_generator.py
@@ -8,12 +8,13 @@ class TXTReportGenerator(ReportGenerator):
         report_table.set_style(MARKDOWN)
         report_table.max_width = 5
         report_table.field_names = self.fields
-        for handler in self.cblock_handlers:
-            for i in range(len(handler.blocks)):
-                if i != len(handler.blocks) - 1:
-                    report_table.add_row(handler.blocks[i].get_record())
+        
+        for one_file_records in self.all_records:
+            for i in range(len(one_file_records)):
+                if i != len(one_file_records) - 1:
+                    report_table.add_row(one_file_records[i])
                 else:
-                    report_table.add_row(handler.blocks[i].get_record(),divider=True)
+                    report_table.add_row(one_file_records[i],divider=True)
         with open(self.name, "w") as report_file:
             report_file.write(report_table.get_string())
 
